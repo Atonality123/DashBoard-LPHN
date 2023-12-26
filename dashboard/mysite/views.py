1c684d7d426f48fb7bcc95a728761c5ad74440fc
Veerapat Sintupong <114942263+0622758452ab@users.noreply.github.com>
2023-12-26 18:35:47 +0700

    all changes is good

--- dashboard/mysite/views.py
+++ dashboard/mysite/views.py
@@ -3,8 +3,28 @@ from django.template import loader
 from .models import Member
 
 
+def coutMember(mymembers):
+    percents = [0, 0, 0, 0]
+
+    for member in mymembers:
+        if member["status"] == "ยังไม่ได้ดำเนินการ":
+            percents[0] += 1
+        elif member["status"] == "ขอเสนออนุมัติ":
+            percents[1] += 1
+        elif member["status"] == "ระหว่างดำเนินงาน":
+            percents[2] += 1
+        elif member["status"] == "ดำเนินงานเสร็จแล้ว":
+            percents[3] += 1
+
+    total = len(mymembers)
+    percents = [int((x / total) * 100) for x in percents]
+
+    return percents
+
+
 def home(request):
     mymembers = Member.objects.all().values()
+    percent = coutMember(mymembers)
     template = loader.get_template("home.html")
-    context = {"mymembers": mymembers}
+    context = {"mymembers": mymembers, "percents": percent}
     return HttpResponse(template.render(context, request))
